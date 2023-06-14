1a043671db11f22f2e0eef3f4fccd2b0669a8ddf
Elena <chailikeg@yahoo.com>
2023-06-14 00:17:44 -0400

    ProjectImplementation-README

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # LibraryManagementSystem
-Library Management System in Java
+### Web Service Course Project
+## Library Management System
+
+
+
+### Project description
+
+The project Library Management System for the course Web Services, takes as basics existing desktop project on Java, that was done in previous Block 3.
+
+Library Management System is created as basic software that has all necessary features for ordinary libraries. It has implemented a connection to database (MySQL). This application has two types of users: Librarian and Student and provides the next functionality:
+for LIBRARIAN: Add Users, Add books, Update books quantity, See "Books" Catalog, See "Available Books" Catalog, See "Issued Books" Catalog, Filter all Catalogs, Sort Catalogs by any field, Issue Books, Return Books, Change Language.
+for USER: See "All Books" Catalog, See "Available Books" Catalog, Filter and Sort Catalogs, Borrow Book from Available, See All "Borrowed Books", Return Books.
+
+
+Web Services should extend desktop applications and implement the possibility to interact through browsers.
+<br/><br/>
+#### Next Web Services planned to be implemented in some steps starting from the Phase1 till the end of the Project:
+<br/>Phase1:
+Show Books Catalog
+Show book’s detailed description
+Show borrowed books by user ID
+Search Book by ISBN
+<br/>Next Phase:
+Add Book
+Issue Book
+Return Book
+Add User
+
+
+Java Client Application will be provided using the implemented REST Web Services.
+
+
+
+
+### Data Input, Output
+
+Data INPUT:
+User ID;
+Book ISBN;
+Links and http addresses
+
+
+Data OUTPUT:
+Books Catalog:
+Book details;
+Borrowed books by User ID;
+User Catalog;
+
+
+### Implemented Business processes
+
+Managing Books is the main task for this application with next processes to be implemented during the Project:
+Add Book;
+Add User;
+Show Catalog;
+Show Book’s information;
+Show Borrowed Books;
+Issue Book;
+Return Book
+
+
+### Data Structure used in the Project
+
+In this Project  has been implemented a connection to database (MySQL). 
+
+
+
--- src/main/java/webLibraryREST/WebLibraryResource.java
+++ src/main/java/webLibraryREST/WebLibraryResource.java
@@ -19,9 +19,9 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("WebLibrary")
 public class WebLibraryResource {
-	
+
 	private DBService dbs;
-	
+
 	/**
 	 * Book Catalog REST HTML
 	 * @return
@@ -29,10 +29,10 @@ public class WebLibraryResource {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String getCatalogHTML() {
-		
+
 		String result;
 		List<Book> catalog = new ArrayList<>();
-		
+
 		dbs = new DBService();
 		try {
 			catalog = dbs.getBookCatalog();
@@ -41,10 +41,10 @@ public class WebLibraryResource {
 			result = "Database Error";
 			System.out.println("Error DB connection");
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * Book Info REST HTML
 	 * @return
@@ -53,10 +53,10 @@ public class WebLibraryResource {
 	@Path("/bookInfo/{isbn}")
 	@Produces(MediaType.TEXT_HTML)
 	public String getBookInfoHTML(@PathParam("isbn") String isbn) {
-		
+
 		String result;
 		Book book;
-		
+
 		dbs = new DBService();
 		try {
 			book = dbs.getByISBN(isbn);
@@ -64,26 +64,34 @@ public class WebLibraryResource {
 		} catch (SQLException | DBConnectionException | SQLExecutingException e) {
 			result = "Database Error";
 		}
-		
+
 		return result;
 	}
-	
+
+	/**
+	 * Method prepares HTML to show Book's details
+	 * @param book
+	 * @return
+	 */
 	private String composeBookHTMLResponse(final Book book) {
-		
+
 		String html = "<table border='0'><tr>"
-				+ "<td colspan=2> Book </td></tr>" 
+				+ "<td colspan=2> Book </td></tr>"
 				+ "<tr><td>Isbn</td><td>"+book.getIsbn()+"</tb></tr>"
 				+ "<tr><td>Title</td><td>"+book.getTitle()+"</tb></tr>"
 				+ "<tr><td>Author</td><td>"+book.getAuthor()+"</tb></tr>"
+				+ "<tr><td>Price</td><td>"+book.getPrice()+"</tb></tr>"
+				+ "<tr><td>Quantity</td><td>"+book.getQuantity()+"</tb></tr>"
+				+ "<tr><td>Date Added</td><td>"+book.getDateAdded()+"</tb></tr>"
 				+ "<tr><td>BookInfo</td><td>"+book.toString()+"</tb></tr>"
 				+ "</table>"
-				
+
 				+"<br><br>"
 				+"<a href=\"http://localhost:8080/LibraryManagementSystem/rest/WebLibrary\"> < Go back to Catalog </a>";
-		
+
 		return html;
 	}
-	
+
 	/**
 	 * Book Catalog REST Text
 	 * @return
@@ -91,24 +99,24 @@ public class WebLibraryResource {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getCatalogText() {
-		
+
 		String result = "";
 		List<Book> catalog = new ArrayList<>();
-		
+
 		dbs = new DBService();
 		try {
 			catalog = dbs.getBookCatalog();
 			for (Book book : catalog)
-			result += book.toString();
+				result += book.toString();
 		} catch (SQLException | DBConnectionException | SQLExecutingException e) {
 			result = "Database Error";
 			System.out.println("Error DB connection");
 		}
-		
+
 		return result;
 	}
-	
-	
+
+
 	/**
 	 * All Issued Books to one User REST JSON
 	 * Method shows all Issued Books to ONE user by it's userID
@@ -119,13 +127,13 @@ public class WebLibraryResource {
 	@Path("/booksToReturn/{user}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<ResponseContainer<Issued, Book, String>> getCatalogJSON(@PathParam("user") int user_id) {
-		
+
 		List<ResponseContainer<Issued, Book, String>> issuedById = new ArrayList<>();
 
 		dbs = new DBService();
 		try {
 			issuedById = dbs.getIssuedToUser(user_id);
-			
+
 		} catch (SQLException | DBConnectionException | SQLExecutingException e) {
 			ResponseContainer<Issued, Book, String> error = new ResponseContainer<>();
 			error.setThird("DB Error");
@@ -134,29 +142,27 @@ public class WebLibraryResource {
 
 		return issuedById;
 	}
-	
+
 	/**
-	 * Method creates Book Catalog table 
+	 * Method creates Book Catalog table
 	 * @param catalog
 	 * @return
 	 */
 	private String composeHTMLResponse(List<Book> catalog) {
-		
+
 		String html = "<table border='1'><tr>"
-				+ "<td colspan=7> Book Catalog </td></tr>" 
+				+ "<td colspan=7> Book Catalog </td></tr>"
 				+ "<tr><td>Isbn</td>"
 				+ "<td>Title</td>"
 				+ "<td>Author</td>"
-				+ "<td>Publisher</td>" 
-				+ "<td>Price</td>"
-				+ "<td>Quantity</td>"
-				+ "<td>DateAdded</td></tr>"
+				+ "<td>Publisher</td>"
+				+ "</tr>"
 				+ composeBookLines(catalog)
 				+ "</table>";
-		
+
 		return html;
 	}
-	
+
 	/**
 	 * Method creates lines for the Book Catalog table
 	 * @param catalog
@@ -167,17 +173,15 @@ public class WebLibraryResource {
 		for(Book book: catalog) {
 			bookLines += "<tr><td>"
 					+ "<a href=\"http://localhost:8080/LibraryManagementSystem/rest/WebLibrary/bookInfo/"
-						+book.getIsbn()
+					+book.getIsbn()
 					+ "\">"
-					+book.getIsbn()+"</a></td>"; 
-			bookLines += "<td>"+book.getTitle()+"</td>"; 
-			bookLines += "<td>"+book.getAuthor()+"</td>"; 
-			bookLines += "<td>"+book.getPublisher()+"</td>"; 
-			bookLines += "<td>"+book.getPrice()+"</td>"; 
-			bookLines += "<td>"+book.getQuantity()+"</td>"; 
-			bookLines += "<td>"+book.getDateAdded()+"</td></tr>"; 
+					+book.getIsbn()+"</a></td>";
+			bookLines += "<td>"+book.getTitle()+"</td>";
+			bookLines += "<td>"+book.getAuthor()+"</td>";
+			bookLines += "<td>"+book.getPublisher()+"</td>";
+			bookLines += "</tr>";
 		}
-		
+
 		return bookLines;
 	}
 
